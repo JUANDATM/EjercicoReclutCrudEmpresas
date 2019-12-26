427747ab20c2c8459ba59705eaa99b1a2201c69e
JUANDATM <kira00711@hotmail.com>
2019-12-26 16:54:32 -0600

    Tablas OK

--- src/EmpresaBundle/Controller/EmpresaController.php
+++ src/EmpresaBundle/Controller/EmpresaController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use EmpresaBundle\Entity\Post;
-use App\Entity\Empresa;
+use EmpresaBundle\Entity\Empresa;
 use App\Repository\EmpresaRepository;
 
 
@@ -47,35 +47,35 @@ use Symfony\Component\Serializer\Serializer;
         $usuario = $repository -> findAll();
         return $this->render('EmpresaBundle:Default:AdminUsuarios.html.twig',array("usuario" => $usuario));
     }
-
-
-
-
-
-
+    private function serializeEmpresa(Empresa $empresa){
+        $encoders = [new XmlEncoder(), new JsonEncoder()];
+        $normalizers = [new ObjectNormalizer()];
+        $serializer = new Serializer($normalizers, $encoders);
+        $jsonContent = $serializer->serialize($empresa, 'json');
+        return $jsonContent;
+    }
     /**
      * @Route("/new", name="empresa_new", methods={"GET","POST"})
      */
-    //public function new(Request $request): Response
-    //{
-    //    $data = $request->request->get('data');
-    //    $empresa = new Empresa();//PROBLEMA
-    //    $empresa->setNombreEmpresa($data['nombre']);
-    //    $empresa->setDireccion($data['direccion']);
-    //    $empresa->setTelefono($data['telefono']);
-    //    $empresa->setDescripcion($data['descripcion']);
-    //    $empresa->setImagen($data['telefono']);
-    //    $existente = $this->getDoctrine()->getRepository('EmpresaBundle:Empresa')->findOneBy(array('id'=>$empresa->getid()));
-    //   if($existente == NULL){
-    //        $entityManager = $this->getDoctrine()->getManager();
-    //        $entityManager->persist($empresa);
-    //        $entityManager->flush();
-    //    }
-    //    $dataResponse = $this->serializeEmpresa($empresa);
-    //    $response = new Response($dataResponse);
-    //    $response->headers->set('Content-Type', 'application/json');
-    //    return $response;
-    //}
+    public function new(Request $request): Response
+    {
+        $data = $request->request->get('data');
+        $empresa = new Empresa();
+        $empresa->setNombreEmpresa($data['nombre']);
+        $empresa->setDireccion($data['direccion']);
+        $empresa->setTelefono($data['telefono']);
+        $empresa->setTelefono($data['descripcion']);
+        $existente = $this->getDoctrine()->getRepository(Empresa::class)->findOneBy(array('id'=>$empresa->getId()));
+        if($existente == NULL){
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($empresa);
+            $entityManager->flush();
+        }
+        $dataResponse = $this->serializeEmpresa($empresa);
+        $response = new Response($dataResponse);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
 
 }
 
